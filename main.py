8b08172c7aa5b953efa15fafb9850b543cb8d65a
truemaxevans <daorodney@gmail.com>
2021-06-10 23:46:48 +0300

    Final commit

--- main.py
+++ main.py
@@ -1,62 +1,57 @@
 from pytube import YouTube
-from playsound import playsound
 import tkinter as tk
 
 WINDOW_WIDTH = 500
-WINDOW_HEIGHT = 200
-WINDOW_TITLE = 'YouTube downloader'
-BUTTON_CLICK_SOUND = 'play'
+WINDOW_HEIGHT = 150
+WINDOW_TITLE = "YouTube downloader"
 
 
-class YouTubeDownloader:
+class YoutubeDownloader:
+
+#Class to create an app that downloads youtube videos
 
     def __init__(self):
         self.window = tk.Tk()
         self.window.geometry("{}x{}".format(WINDOW_WIDTH, WINDOW_HEIGHT))
-        self.window.configure(bg='#a9dcd6')
+        self.window.configure(bg="#509997")
         self.window.title(WINDOW_TITLE)
 
-        # Create labels
-        self.link_lable = tk.Label(self.window, text='Download link')
-        self.link_lable.grid(column=0, row=0)
-        self.link_lable = tk.Label(self.window, text='Save file as')
-        self.link_lable.grid(column=0, row=1)
-        self.link_lable = tk.Label(self.window, text='Save PATH')
-        self.link_lable.grid(column=0, row=2)
-        self.link_lable = tk.Label(self.window, text='File extension')
-        self.link_lable.grid(column=0, row=3)
+        self.link_label = tk.Label(self.window, text="Download Link")
+        self.link_label.grid(column=0, row=0)
+        self.name_label = tk.Label(self.window, text="Save File as")
+        self.name_label.grid(column=0, row=1)
+        self.path_label = tk.Label(self.window, text="Save File Path")
+        self.path_label.grid(column=0, row=2)
+        self.ext_label = tk.Label(self.window, text="File extension")
+        self.ext_label.grid(column=0, row=3)
 
-        # Create entry fields
-        self.link_entry = tk.Entry(master=self.window, width=60)
+        self.link_entry = tk.Entry(master=self.window, width=40)
         self.link_entry.grid(column=1, row=0)
-        self.link_entry = tk.Entry(master=self.window, width=60)
-        self.link_entry.grid(column=1, row=1)
-        self.link_entry = tk.Entry(master=self.window, width=60)
-        self.link_entry.grid(column=1, row=2)
-        self.link_entry = tk.Entry(master=self.window, width=60)
-        self.link_entry.grid(column=1, row=3)
-
-        # Download button
-        self.download_button = tk.Button(self.window, text='Submit', command=self.__get_link)
+        self.name_entry = tk.Entry(master=self.window, width=40)
+        self.name_entry.grid(column=1, row=1)
+        self.path_entry = tk.Entry(master=self.window, width=40)
+        self.path_entry.grid(column=1, row=2)
+        self.ext_entry = tk.Entry(master=self.window, width=40)
+        self.ext_entry.grid(column=1, row=3)
+        self.download_button = tk.Button(self.window, text="Download", command=self.__get_link)
         self.download_button.grid(column=1, row=4)
 
         return
 
-    def downloader(self, link, save_path='', save_name='', extension='mp4'):
+    def __downloader(self, link, save_path="", save_name="", extension="mp4"):
         yt = YouTube(link)
-        yt.streams.filter(progressive=True, file_extension=extension).order_by('resolution').desc().first()
-        yt.streams.download(output_path=save_path, filename=save_name)
+        yt_stream = yt.streams.filter(progressive=True, file_extension=extension).order_by('resolution').desc().first()
+        yt_stream.download(output_path=save_path, filename=save_name)
 
         return
 
     def __get_link(self):
-        playsound(BUTTON_CLICK_SOUND)
         link = self.link_entry.get()
-        path = self.link_entry.get()
-        name = self.link_entry.get()
-        ext = self.link_entry.get()
+        path = self.path_entry.get()
+        name = self.name_entry.get()
+        ext = self.ext_entry.get()
 
-        self.downloader(link, path, name, ext)
+        self.__downloader(link, path, name, ext)
 
         return
 
@@ -65,6 +60,6 @@ class YouTubeDownloader:
         return
 
 
-if __name__ == '__main__':
-    app = YouTubeDownloader()
+if __name__ == "__main__":
+    app = YoutubeDownloader()
     app.run_app()
